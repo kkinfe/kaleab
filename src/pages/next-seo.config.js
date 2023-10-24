0c7a94bc880977108507568a89841cdf6dc5b9ae
kkinfe <kaleabkinfe04@gmail.com>
2023-10-24 12:14:36 -0400

    Update next-seo.config.js

--- src/pages/next-seo.config.js
+++ src/pages/next-seo.config.js
@@ -1,13 +1,14 @@
+// eslint-disable-next-line
 export default {
-    openGraph: {
-      type: 'website',
-      locale: 'en_IE',
-      url: 'https://kinfe.github.io',
-      siteName: 'Kaleab Kinfe',
-    },
-    twitter: {
-      handle: '@Kaleabkinfe',
-      site: '@Kaleabkinfe',
-      cardType: 'summary_large_image',
-    },
-  };
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://kinfe.github.io",
+    siteName: "Kaleab Kinfe",
+  },
+  twitter: {
+    handle: "@Kaleabkinfe",
+    site: "@Kaleabkinfe",
+    cardType: "summary_large_image",
+  },
+};
