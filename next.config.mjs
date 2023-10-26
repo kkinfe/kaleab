82f44466fb1300591f46277f2a932a3fad756728
kkinfe <kaleabkinfe04@gmail.com>
2023-10-26 08:18:06 -0400

    Finilazing

--- next.config.mjs
+++ next.config.mjs
@@ -8,12 +8,10 @@ import rehypePresetMinify from 'rehype-preset-minify'
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx'],
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {    
-    scrollRestoration: true,
-  },
+  swcMinify: true, 
 }
 const withPWA = nextPWA({
+  disable: process.env.NODE_ENV === 'development',
   dest: "public",
   register: true,
   skipWaiting: true,
