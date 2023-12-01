9990b088a447d897efb2f7789850ba0a4cd07c94
kkinfe <kaleabkinfe04@gmail.com>
2023-12-01 12:41:21 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -56,7 +56,7 @@ function MobileNavigation(props) {
         aria-label="menu"
         className="items-center rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20"
       >
-        <HamburgernIcon className="h-4 w-4  dark:group-hover:stroke-zinc-400" />
+        <HamburgernIcon className="h-4 w-4  text-zinc-500 dark:text-zinc-400" />
       </Popover.Button>
       <Transition.Root>
         <Transition.Child
